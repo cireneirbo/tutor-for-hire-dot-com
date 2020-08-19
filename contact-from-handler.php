1500e97357f830167d334695fbfe9a51233bd80f
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-08-19 03:51:07 -0400

    trying new php form

---
+++ contact-from-handler.php
@@ -0,0 +1,25 @@
+<?php
+
+    $name = $_POST['$name'];
+    $visitor_email = $_POST['$email'];
+    $message = $_POST['$message'];
+
+    $email_from = "ejobrien315@gmail.com";
+
+    $email_subject = "New Form";
+
+    $email_body = "User name: $name.\n".
+                    "User email: $visitor_email. \n".
+                        "User Message: $message. \n";
+
+    $to = "ejobrien315@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+
+?>
