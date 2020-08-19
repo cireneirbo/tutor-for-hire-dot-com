62694168a976e73fe40bc2fa7c3b8ccad72299a4
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-08-19 02:59:47 -0400

    updated index to use php

--- index.php
+++ index.php
@@ -1,3 +1,80 @@
+<?php
+if (isset($_POST['Email'])) {
+
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "ejobrien315@gmail.com";
+    $email_subject = "testing testing";
+
+    function problem($error)
+    {
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br><br>";
+        echo $error . "<br><br>";
+        echo "Please go back and fix these errors.<br><br>";
+        die();
+    }
+
+    // validation expected data exists
+    if (
+        !isset($_POST['Name']) ||
+        !isset($_POST['Email']) ||
+        !isset($_POST['Message'])
+    ) {
+        problem('We are sorry, but there appears to be a problem with the form you submitted.');
+    }
+
+    $name = $_POST['Name']; // required
+    $email = $_POST['Email']; // required
+    $message = $_POST['Message']; // required
+
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+    if (!preg_match($email_exp, $email)) {
+        $error_message .= 'The Email address you entered does not appear to be valid.<br>';
+    }
+
+    $string_exp = "/^[A-Za-z .'-]+$/";
+
+    if (!preg_match($string_exp, $name)) {
+        $error_message .= 'The Name you entered does not appear to be valid.<br>';
+    }
+
+    if (strlen($message) < 2) {
+        $error_message .= 'The Message you entered do not appear to be valid.<br>';
+    }
+
+    if (strlen($error_message) > 0) {
+        problem($error_message);
+    }
+
+    $email_message = "Form details below.\n\n";
+
+    function clean_string($string)
+    {
+        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+        return str_replace($bad, "", $string);
+    }
+
+    $email_message .= "Name: " . clean_string($name) . "\n";
+    $email_message .= "Email: " . clean_string($email) . "\n";
+    $email_message .= "Message: " . clean_string($message) . "\n";
+
+    // create email headers
+    $headers = 'From: ' . $email . "\r\n" .
+        'Reply-To: ' . $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    @mail($email_to, $email_subject, $email_message, $headers);
+?>
+
+    <!-- include your success message below -->
+
+    Thank you for contacting us. We will be in touch with you very soon.
+
+<?php
+}
+?>
+
 <!DOCTYPE HTML>
 <!--
 	Solid State by HTML5 UP
@@ -44,7 +121,7 @@
 						<div class="inner">
 							<!-- <div class="logo"><span class="icon fa-gem"></span></div> -->
 							<h2>Susan O'Brien's Tutoring</h2>
-							<h3>Meeting your child's educational needs in Sarasota and Bradenton, FL</h3>
+							<p><b>Meeting your child's educational needs in Sarasota and Bradenton, FL</b></p>
 							<p>My mission is to provide a quality tutoring that will support your child's learning and will empower him/her to achieve success in a safe, welcoming, and caring environment.</p>
 						</div>
 					</section>
@@ -92,7 +169,7 @@
 									<a href="#" class="image"><img src="images/classrom2.jpg" alt="" /></a>
 									<div class="content">
 										<h2 class="major">Areas of Expertise</h2>
-										<h5>My services include:</h5>
+										<p><b>My services include:</b></p>
 										<p><b>Grades K-5th</b> tutoring in reading, writing, and math. I will specifically focus on FSA standards to assist your child in being successful on state assessments.
 										</p>
 										<p><b>Grades 6 -12th</b> tutoring in reading and English with a focus on FSA English and SAT.</p>
@@ -168,7 +245,7 @@
 											<p>The best option is using my Zoom channel for no-contact instruction. Zoom is a web-service with screen-sharing capabilities which has been utilized by the Manatee School District, among many others.<br>
 												Also, provided safety precautions are considered, I may be available to tutor in a public space, such as a library or cafe.<br>
 											</p>
-											<h5>Currently, I am not making home visits due to the COVID-19 epidemic.</h5>
+											<p><b>Currently, I am not making home visits due to the COVID-19 epidemic.</b></p>
 											<!-- <a href="#" class="special">Learn more</a> -->
 										</article>
 									</section>
@@ -185,19 +262,19 @@
 						<div class="inner">
 							<h2 class="major">Get in touch</h2>
 							<p>Please contact me with any inquiries. I will be happy to discuss my credentials and our tutoring options.</p>
-							<form method="post" action="#">
+							<form method="post" action="">
 								<div class="fields">
 									<div class="field">
 										<label for="name">Name</label>
-										<input type="text" name="name" id="name" />
+										<input type="text" name="Name" id="name" />
 									</div>
 									<div class="field">
 										<label for="email">Email</label>
-										<input type="email" name="email" id="email" />
+										<input type="email" name="Email" id="email" />
 									</div>
 									<div class="field">
 										<label for="message">Message</label>
-										<textarea name="message" id="message" rows="4"></textarea>
+										<textarea name="Message" id="message" rows="4"></textarea>
 									</div>
 								</div>
 								<ul class="actions">
